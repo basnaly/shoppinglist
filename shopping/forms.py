e1a1c6377114d161b172f1cc6ce692a3c83edd24
basnaly <basnaly@gmail.com>
2023-12-18 13:42:06 +0200

    Create superuser, add register and login functions forms and html files

---
+++ shopping/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import User
+from django.contrib.auth import password_validation 
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}), label='')
+    email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control'}), label='')
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class': 'form-control'}), label='')
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class': 'form-control'}), label='')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}), label='', validators=[password_validation.validate_password])
+    confirmation =forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirmation', 'class': 'form-control'}), label='', validators=[password_validation.validate_password])
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control mb-3'}), label='')
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control mb-3'}), label='')
--- shopping/views.py
+++ shopping/views.py
@@ -1,8 +1,86 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
+from shopping.forms import RegisterForm, LoginForm
+from .models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.db import IntegrityError
 
 # Create your views here.
 
 def index(request):
     return render(request, "shopping/index.html",{})
+
+
+def register(request):
+    if request.method == "GET":
+        return render(request, "shopping/register.html", {
+            "form": RegisterForm
+        })
+    else:
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            password = form.cleaned_data["password"]
+            confirmation = form.cleaned_data["confirmation"]
+            
+            if password != confirmation:
+                messages.error(request, "Password must match conformation.")
+                return HttpResponseRedirect(reverse("register"))
+                
+            try:
+                new_user = User.objects.create_user(
+                    username = username,
+                    email = email,
+                    first_name = first_name,
+                    last_name = last_name,
+                    password = password
+                )
+                new_user.save()
+            except IntegrityError:
+                messages.error(request, "User already taken!")
+                return HttpResponseRedirect(reverse("register"))
+            
+            login(request, new_user)
+            messages.success(request, "The user was successfully saved!")
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            print(form.errors)
+            messages.error(request, "Some of the values are not valid!")
+            return HttpResponseRedirect(reverse("register"))
+                
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "shopping/login.html", {
+            "form": LoginForm
+        })
+    else:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+            else:
+                messages.error(request, "Invalid username or password!")
+                return render(request, "shopping/login.html", {
+                    "form":form
+                })
+        else:
+            messages.error(request, "The form is not valid!")
+            return render(request, "shopping/login.html", {
+                "form":form
+            })
+
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
